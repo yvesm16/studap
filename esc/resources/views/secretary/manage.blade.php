1875adb3974502226f7ba8dbed235db629b1f039
yves <yves.mabelin.iics@ust.edu.ph>
2021-11-22 20:22:25 +0800

    design changes in manage user

--- esc/resources/views/secretary/manage.blade.php
+++ esc/resources/views/secretary/manage.blade.php
@@ -26,40 +26,9 @@
               <h1 class="page-header"><span id="titlePage">Manage User</span></h1>
           </div>
       </div>
-      <div class="row">
-          <div class="col-lg-6 col-md-6">
-              <div class="panel panel-warning">
-                  <div class="panel-heading">
-                      <div class="row">
-                          
-                      </div>
-                  </div>
-                      <div class="panel-footer" style="background-color: white !important">
-                          
-                      </div>
-                  </a>
-              </div>
-          </div>
-          <div class="col-lg-6 col-md-6">
-              <div class="panel panel-success">
-                  <div class="panel-heading">
-                      <div class="row">
-                          <div class="col-xs-3">
-                          </div>
-                          <div class="col-xs-9 text-right">
-                              
-                          </div>
-                      </div>
-                  </div>
-                      <div class="panel-footer" style="background-color: white !important">
-                      
-                      </div>
-                  </a>
-              </div>
-          </div>
-      </div>
+      
       {{-- <div class="row downloadReportDiv" style="margin-bottom: 1%; text-align: right"> --}}
-      </div>
+      
       <div class="row">
         <div class="col-lg-12">
             <div class="panel panel-default">
